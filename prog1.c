a74a9203cd69fdd5c412241a6c1bf7a02ed19320
Varsha Karumuru <karumuva@oregonstate.edu>
2026-01-19 23:20:01 -0800

    Assignment 1 complete

--- prog1.c
+++ prog1.c
@@ -1,11 +1,25 @@
 #include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
-
+#include <stdlib.h>  // Necessary for rand, srand
+#include <time.h>    // Necessary for time
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
+        // The main function is the entrypoint of the program. When the program
+        // is executed, it will automatically start here.
 	
-	// TODO Complete the program
+	
+	srand(time(NULL));	//random = min + rand() % (max - min + 1);
+        int myrand = 0 + rand() % (11);
+        
+        if (myrand <= 4) {
+                printf("Eat more beef, kick less cats\n");
+        }
+
+        else if (myrand >= 5 && myrand <= 9) {
+                printf("FRODO LIVES\n");
+	}
+	else {
+		printf("Larn is the best roguelike\n");
+	}
+
+	printf("The random number was:%d\n", myrand);
 }
